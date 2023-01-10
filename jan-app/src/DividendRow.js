f88bab208fc71500ef501bbd1356b6313096ef27
asolanky <alwinsolanky@gmail.com>
2023-01-10 13:23:05 +0000

    Very Basic React App

---
+++ jan-app/src/DividendRow.js
@@ -0,0 +1,16 @@
+// DividendRow.js
+
+import React, { Component } from 'react';
+
+class DividendRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.name}</td>
+        <td>{this.props.dividendYield}</td>
+      </tr>
+    );
+  }
+}
+
+export default DividendRow;
